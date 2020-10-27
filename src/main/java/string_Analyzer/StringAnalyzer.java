2a395b660683aefa474c175833823383662983ec
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-10-27 14:50:20 +0100

    added counting numbers in given string
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/main/java/string_Analyzer/StringAnalyzer.java
+++ src/main/java/string_Analyzer/StringAnalyzer.java
@@ -11,6 +11,7 @@ public class StringAnalyzer {
 		StringBuilder stringBuilder = new StringBuilder("String ");
 		int howManyLowerCase = 0;
 		int howManyUpperCase = 0;
+		int howManyNumbers = 0;
 		for (int i = 0; i < var.length(); i++) {
 			if (Character.isLowerCase(var.charAt(i))) {
 				howManyLowerCase++;
@@ -18,13 +19,19 @@ public class StringAnalyzer {
 			else if (Character.isUpperCase(var.charAt(i))) {
 				howManyUpperCase++;
 			}
+			else if (Character.isDigit(var.charAt(i))){
+				howManyNumbers++;
+			}
 		}
 
 		if (howManyLowerCase > 0) {
-			stringBuilder.append("contains ").append(howManyLowerCase).append(" Lowercase letters");
+			stringBuilder.append("contains ").append(howManyLowerCase).append(" Lowercase letters ");
 		}
 		if (howManyUpperCase > 0) {
-			stringBuilder.append("contains ").append(howManyUpperCase).append(" Uppercase letters");
+			stringBuilder.append("contains ").append(howManyUpperCase).append(" Uppercase letters ");
+		}
+		if (howManyUpperCase > 0) {
+			stringBuilder.append("contains ").append(howManyNumbers).append(" numbers ");
 		}
 
 		return stringBuilder.toString();
