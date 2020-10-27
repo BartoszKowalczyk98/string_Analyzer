f4eaa2e94fb69e4891c80c4a7addb1dcb74e554e
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-10-27 14:23:27 +0100

    Initial commit whole project build with spring initializr, with options: Maven project Java language Spring Boot 2.3.4 packaging Jar Java 11 and with additional dependencies Spring Web Spring Boot DevTools

---
+++ src/main/java/string_Analyzer/StringAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package string_Analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringAnalyzerApplication.class, args);
+	}
+
+}
