fd787d6fc80e75bc429bf89cc74725a7d47be945
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-10-27 14:45:11 +0100

    added counting uppercase letters
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/main/java/string_Analyzer/StringAnalyzer.java
+++ src/main/java/string_Analyzer/StringAnalyzer.java
@@ -8,17 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class StringAnalyzer {
 	@RequestMapping(path = "/analyze/{var}")
 	public String analyzeInput(@PathVariable String var) {
-		StringBuilder stringBuilder = new StringBuilder();
+		StringBuilder stringBuilder = new StringBuilder("String ");
 		int howHowManyLowerCase = 0;
+		int howHowManyUpperCase = 0;
 		for (int i = 0; i < var.length() ; i++) {
 			if (Character.isLowerCase(var.charAt(i))){
 				howHowManyLowerCase++;
+			}if (Character.isUpperCase(var.charAt(i))){
+				howHowManyUpperCase++;
 			}
 		}
 
 		if (howHowManyLowerCase>0){
 			stringBuilder.append("contains ").append(howHowManyLowerCase).append(" Lowercase letters");
 		}
+		if (howHowManyUpperCase>0){
+			stringBuilder.append("contains ").append(howHowManyUpperCase).append(" Uppercase letters");
+		}
+
 		return stringBuilder.toString();
 	}
 }
